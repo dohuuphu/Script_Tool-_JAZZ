0ffc531d853c4affe2c0cf86431675e524fd1195
DL\pdo2 <phu.do2@datalogic.com>
2020-09-30 14:01:24 +0700

    asdasd

--- testSElenium/Add_lib.py
+++ testSElenium/Add_lib.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import variable
+from variable import *
 import time
 
 
@@ -15,31 +15,41 @@ driver.maximize_window()
 
 
 def Click_button_id(timeout_item, path_item):
-    WebDriverWait(driver, timeout_item).until(EC.presence_of_element_located((By.ID, path_item)))        # wait until item exits
+    WebDriverWait(driver, timeout_item).until(EC.presence_of_element_located((By.LINK_TEXT, path_item[0])), message=("Can not find: " + path_item[1]))        # wait until item exits
     item =  driver.find_element_by_id(path_item)
     item.click()
 
-def Click_button_xpath(timeout_item, path_item):
-    WebDriverWait(driver, timeout_item).until(EC.presence_of_element_located((By.ID, path_item)))        # wait until item exits
+def Click_button_xpath(timeout_item, path_item):  
+    WebDriverWait(driver, timeout_item).until(EC.presence_of_element_located((By.LINK_TEXT, path_item[0])), message=("Can not find: " + path_item[1]))        # wait until item exits
     item =  driver.find_element_by_xpath(path_item)
+    # get = item.get_attribute("type")
+    # print(get)
     item.click()
+
+def Click_LinkText(timeout_item, path_item):
+    WebDriverWait(driver, timeout_item).until(EC.presence_of_element_located((By.LINK_TEXT, path_item[0])), message=("Can not find: " + path_item[1]))        # wait until item exits
+    item =  driver.find_element_by_link_text(path_item)
+    item.click()
+    
 
 def Send_key_id(timeout_item, path_item, string):
-    WebDriverWait(driver, timeout_item).until(EC.presence_of_element_located((By.ID, path_item)))        # wait until item exits
+    WebDriverWait(driver, timeout_item).until(EC.presence_of_element_located((By.LINK_TEXT, path_item[0])), message=("Can not find: " + path_item[1]))        # wait until item exits
     item =  driver.find_element_by_id(path_item)
     item.send_keys(string)
+    
 
 def Send_key_xpath(timeout_item, path_item, string):
-    WebDriverWait(driver, timeout_item).until(EC.presence_of_element_located((By.ID, path_item)))        # wait until item exits
+    WebDriverWait(driver, timeout_item).until(EC.presence_of_element_located((By.LINK_TEXT, path_item[0])), message=("Can not find: " + path_item[1]))        # wait until item exits
     item =  driver.find_element_by_xpath(path_item)
     item.send_keys(string)
+    
 
 # def login():  
-#     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, variable.IDuser_id)))        # wait until IDbox exits
+#     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, IDuser_id)))        # wait until IDbox exits
 #     #Find path
-#     IDuser = driver.find_element_by_id(variable.IDuser_id)
-#     pass_login = driver.find_element_by_id(variable.password_id)
-#     login_button = driver.find_element_by_xpath(variable.login_button_xpath)
+#     IDuser = driver.find_element_by_id(IDuser_id)
+#     pass_login = driver.find_element_by_id(password_id)
+#     login_button = driver.find_element_by_xpath(login_button_xpath)
     
 #     # Edit login
 #     IDuser.send_keys( "lhoang")
@@ -50,18 +60,25 @@ def Send_key_xpath(timeout_item, path_item, string):
 #     ID_test.click()
 
 def login():
-    Send_key_id(30,variable.IDuser_id,variable.login_ID)
-    Send_key_id(30, variable.password_id, variable.login_password)
-    Click_button_xpath(30,variable.login_button_xpath)
+    Send_key_id(timeout,IDuser_id,login_ID)
+    Send_key_id(timeout, password_id, login_password)
+    #Click_button_xpath(timeout,login_button_xpath)
+    Click_LinkText(timeout, ID_test_id)
+
 
 
 def Click_Planning():
-     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, variable.Plannningbox_id)))        # wait until planning box exits
-     Planning_box = driver.find_elements_by_id(variable.Plannningbox_id)
-     Planning_box.click()
+    WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.ID, Plannningbox_id)))        # wait until planning box exits
+    Planning_box = driver.find_elements_by_id(Plannningbox_id)
+    Planning_box.click()
+     
     
+def test():
+     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, Plannningbox_id)))        # wait until planning box exits
+
 def main():
     login()
+    Click_Planning()
 
 if __name__ == "__main__":
     main()
--- testSElenium/variable.py
+++ testSElenium/variable.py
@@ -1,10 +1,11 @@
 #Const
 login_ID = "lhoang"
 login_password = "D@talogic7"
+timeout = 5
 
 #path
-IDuser_id = "jazz_app_internal_LoginWidget_0_userId"
-password_id = "jazz_app_internal_LoginWidget_0_password"
-login_button_xpath = "//*[@id=\"jazz_app_internal_LoginWidget_0\"]/div[1]/div[1]/div[3]/form/button"
-ID_test_id = "jazz_ui_ResourceLink_0"
-Plannningbox_id = "jazz_ui_MenuPopup_7"
+IDuser_id = ["jazz_app_internal_LoginWidget_0_userId", "IDuser_id"]
+password_id = ["jazz_app_internal_LoginWidget_0_password", "password_id"]
+login_button_xpath = ["//*[@id=\"jazz_app_internal_LoginWidget_0\"]/div[1]/div[1]/div[3]/form/button", "login_button_xpath"]
+ID_test_id = ["ID (Test)","ID (test)"]
+Plannningbox_id = ["jazz_ui_MenuPopup_7", "Plannningbox_id"]
