6b34145cbf1bbc66de05ea3f721869ddc9c2ae6e
Duy Nguyen <nnduy98@gmail.com>
2020-10-05 09:12:28 +0700

    Get DATA

--- testSElenium/Get_dataCsv.py
+++ testSElenium/Get_dataCsv.py
@@ -5,6 +5,7 @@ Name_Colum =  2
 x = 0
 Check_row= 0
 Note = "True"
+
 def CheckActive(FileName,num,colum):
         csv = pd.read_csv(FileName)
         row = csv[csv.Num == num].values[0]
@@ -17,7 +18,7 @@ def GetDataName(FileName,num,colum):
         row = csv[csv.Num == num].values[0]
         Check_row = row[colum]
         # print (row[colum])
-        Check_row = convert(Check_row))
+        #Check_row = convert(Check_row))
         if  Check_row == Note  :
                 print (Check_row)
         # print (Check_row)
@@ -25,10 +26,23 @@ def GetDataName(FileName,num,colum):
 
 for i in range(5):
         x = x+1
-        GetDataName('Data_Main.csv', x ,Check_Active_Machine_Colum)
+        A = CheckActive('Data_Main.csv',x,Check_Active_Machine_Colum)
+        print ('AAA is', A)
+        print(A,'is',bool(A))
+        # if A(True):
+        #         print('MAchine',CheckActive('Data_Main.csv',x,2))
+        # else:
+        #         print('MAchine Not equal ',CheckActive('Data_Main.csv',x,2))
+        # GetDataName('Data_Main.csv', x ,Check_Active_Machine_Colum)
+
         # print(Status)
         # print (Check_row)
         # GetDataName('Data_Main.csv', x ,Name_Colum)
+        # print (Check_Active_Machine_Colum)
+# CheckActive('Data_Main.csv',1,3)
+# CheckActive('Data_Main.csv',1,5)
+# A = CheckActive('Data_Main.csv',2,5)
+# print ('AAA', A)
 
 # else:
 #   print("Finally finished!")
