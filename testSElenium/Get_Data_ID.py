b12446722b02f135cf6db4fa12cb858487a0a733
Duy Nguyen <nnduy98@gmail.com>
2020-10-05 10:52:27 +0700

    Complete get id name

--- testSElenium/Get_Data_ID.py
+++ testSElenium/Get_Data_ID.py
@@ -1,21 +1,14 @@
 import csv
 import pandas as pd
-Check_Machine_Colum = 1 
-Name_Colum =  2
-Total_Test = 5
-x = 0
-Check_row= 0
-def Read_Data_ID():
+def Read_Data_ID(input_Name):
     with open('Data_ID.csv', 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             name = row['Name']
             idxpath = row['ID_XPath']
-            # print(f"{name} - {idxpath} ")
-            print("Row :      ", row)
-            print("Read:      ", reader)
-            print("Name : ", name)
-            print("idXpath : " ,idxpath)
-        # print ("reader",reader)
+            if input_Name== name:
+                print("Name : ", name)
+                print("idXpath : " ,idxpath)
 
-Read_Data_ID()
+Read_Data_ID("IDuser_id")
+Read_Data_ID("Plannningbox_id")
