71cf9d92563e88c751e3482e39786a79fa454b30
DHP\Đỗ Hữu Phú <phu.do2@datalogic.com>
2020-10-08 21:47:57 +0700

    edit_gui

--- testSElenium/Tkinter_GUI/main.py
+++ testSElenium/Tkinter_GUI/main.py
@@ -3,20 +3,16 @@ import tkinter as tk
 from tkinter import  ttk, messagebox
 
 def show_message():
-    messagebox.showinfo(title= 'input info', message= lib.plus())
+    messagebox.showinfo(title= 'input info', message= lib.plus)
+
+
 
 def setup_win():
-    win = tk.Tk()
-    win.title("Script Tool")
+    pass
 
-    #create label
-    lable = ttk.Label(win, text = "222")
-    lable.grid(column = 100 , row = 100)
-    #create button
-    button = ttk.Button(win, text= 'run', command = show_message )
-    button.grid(column = 1, row = 0)
-    win.mainloop()
-
+def get_entry():
+    id_entry = entry_id.get()
+    print((id_entry))
 
 def main():
     setup_win()
@@ -26,4 +22,41 @@ def main():
 
 
 if __name__ == "__main__":
+    win = tk.Tk()
+    win.title("Script Tool")
+    win.geometry("280x160")
+    # frame = ttk.Frame(win, width = 300, height = 250)
+    # frame.grid(win)
+
+    # create label
+    label_content = ttk.Label(win, text = " Run Test Plan On Jazz_page" )
+    label_id = ttk.Label(win, text="ID User:")
+    label_pass = ttk.Label(win, text="Password:")
+    label_TestPlan_id = ttk.Label(win, text="TestPlan ID:")
+    label_content.place(relx=0.5, rely=0.05, anchor=tk.N)
+    label_id.place(relx=0.20, rely=0.25, anchor=tk.N)
+    label_pass.place(relx=0.20, rely=0.45, anchor=tk.N)
+    label_TestPlan_id.place(relx=0.20, rely=0.65, anchor=tk.N)
+   # lable_pass.pack(win, sticky = tk.E + tk.W)
+
+    # create text entry
+    entry_id = ttk.Entry(win)
+    entry_pass = ttk.Entry(win)
+    entry_Testplan = ttk.Entry(win)
+    # entry_id.grid(row=3, column=3)
+    # entry_pass.grid(row=5, column=3)
+    # entry_Testplan.grid(row=7, column=3)
+    entry_id.place(relx=0.65, rely=0.25, anchor=tk.N)
+    entry_pass.place(relx=0.65, rely=0.45, anchor=tk.N)
+    entry_Testplan.place(relx=0.65, rely=0.65, anchor=tk.N)
+
+    # create button
+    button = ttk.Button(win, text='Run', command=get_entry)
+    button.place(relx=0.5, rely=1, anchor = tk.S)
+
+    # create text entry
+    # text_entry = ttk.ent Entry(win, width= 20, bg ="black")
+    # text_entry.grid(column = 2, row = 0)
+    # loop
+    win.mainloop()
     main()
